f69b61808899f53cebe38914cac073f236995248
AmitLustiger <amit@mabaya.com>
2025-02-17 23:27:53 +0200

    album router included

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
-import os
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import events, guests, albums, auth, payment
 
 # Initialize the FastAPI app
@@ -12,6 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # React local dev
+        "http://127.0.0.1:8000",  # React local dev
         "http://photoguests.com",  # Production frontend
         "https://photoguests.com",  # Production frontend
     ],
@@ -23,7 +23,7 @@ app.add_middleware(
 # Include routers for different sections of the app
 app.include_router(events.router, prefix="/events", tags=["events"])
 app.include_router(guests.router, prefix="/guests", tags=["guests"])
-app.include_router(guests.router, prefix="/albums", tags=["albums"])
+app.include_router(albums.router, prefix="/albums", tags=["albums"])
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(payment.router, prefix="/payment", tags=["payment"])  # Corrected this line!
 
