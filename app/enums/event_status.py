58b346180937843ed1769896fea5bb103c7c3004
AmitLustiger <amit@mabaya.com>
2025-03-05 23:05:59 +0200

    change event statuses to hebrew

--- app/enums/event_status.py
+++ app/enums/event_status.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
-
 class EventStatus(str, Enum):
-    PENDING_UPLOAD = "Pending Upload"
-    ALBUM_UPLOADED = "Album Uploaded"
-    COMPLETED = "Completed"
+    PENDING_UPLOAD = "ממתין להעלאה"
+    ALBUM_UPLOADED = "אלבום הועלה"
+    COMPLETED = "האלבומים נשלחו לאורחים"
